279de68e11612f8332a42f96566dfc9a3e84484d
Chaitra <ravurisaichaitra@gmail.com>
2022-01-10 10:53:54 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Row from './Row'
+import requests from './requests'
+import Banner from './Banner.js'
+import Nav from './Nav'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+      <Banner/>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTrending}/>
+      <Row title="Action movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentries" fetchUrl={requests.fetchDocumentries}/>
     </div>
   );
 }
